1a5a3bb90e7079e3a7e81ba82300e7099ed79a34
Anderson <andersonfrota@outlook.pt>
2026-03-03 23:26:47 +0000

    Fix duplicated server code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
@@ -8,25 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // IMPORTAR ROTAS
 const produtosRoutes = require("./routes/produtos");
 const importarRoutes = require("./routes/importar");
 const authRoutes = require("./routes/auth");
 
-// ROTAS API
+// ROTAS
 app.use("/produtos", produtosRoutes);
 app.use("/importar", importarRoutes);
 app.use("/auth", authRoutes);
-
-//ROTA SPA
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/index.html"));
-});
 
 // PORTA
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
