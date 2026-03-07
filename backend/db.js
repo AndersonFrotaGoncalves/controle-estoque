87476a6d0b37994b7da11d28310438826ad6378a
Anderson <andersonfrota@outlook.pt>
2026-03-07 00:18:47 +0000

    corrigindo senha banco railway

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
   host: "maglev.proxy.rlwy.net",
   user: "root",
   password: "123456",
-  database: "railway",
+  database: "goJcMRFGnYcqYZltvSblgdFwVDmAaNcg",
   port: 50021,
   waitForConnections: true,
   connectionLimit: 10,
