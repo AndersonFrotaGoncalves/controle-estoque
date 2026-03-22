d4681fffee665abdd45defece29d7e3300f5b67a
Anderson <andersonfrota@outlook.pt>
2026-03-22 13:17:27 +0000

    fix start script render

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,160 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-console.log("🚀 SERVER STARTANDO...");
+const db = require("./database"); // 🔥 ESSENCIAL
 
 const app = express();
 
+// rotas
+const authRoutes = require("./routes/auth");
+const usuariosRoutes = require("./routes/usuarios");
+const importarRoutes = require("./routes/importar");
+const movimentacoes = require("./routes/movimentacoes");
+const produtos = require("./routes/produtos");
+const sapRoutes = require("./routes/sap");
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+//rotas API
+app.use("/api", movimentacoes);
+app.use("/api", produtos);
+app.use("/api", sapRoutes);
+
+
+// outras rotas
+app.use("/auth", authRoutes);
+app.use("/usuarios", usuariosRoutes);
+app.use("/importar", importarRoutes);
+
+// frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.get("/", (req, res) => {
-  res.send("OK RENDER 🚀");
+  res.sendFile(path.join(__dirname, "../frontend/login.html"));
 });
 
+// porta
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("✅ Rodando na porta", PORT);
+  console.log("Servidor rodando na porta", PORT);
 });
+
+/* PÁGINA INICIAL */
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/login.html"));
+});
+
+/* ===============================
+   LISTAR PRODUTOS
+================================ */
+
+app.get("/produtos", (req, res) => {
+
+  db.query("SELECT * FROM produtos", (err, result) => {
+
+    if (err) {
+      console.log("ERRO MYSQL:", err);
+      return res.status(500).json(err);
+    }
+
+    res.json(result);
+
+  });
+
+});
+
+/* ===============================
+   CADASTRAR PRODUTO
+================================ */
+
+app.post("/produtos", (req, res) => {
+
+  const { codigo, descricao, rack, nivel, quantidade, quantidade_minima } = req.body;
+
+  const sql = `
+    INSERT INTO produtos
+    (codigo, descricao, rack, nivel, quantidade, quantidade_minima)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [codigo, descricao, rack, nivel, quantidade, quantidade_minima || 2],
+    (err) => {
+
+      if (err) {
+        return res.status(500).json({ error: "Erro ao cadastrar produto" });
+      }
+
+      res.json({ sucesso: true });
+
+    }
+  );
+
+});
+
+/* ===============================
+   EDITAR PRODUTO
+================================ */
+
+app.put("/produtos/:id", (req, res) => {
+
+  const { id } = req.params;
+
+  const { codigo, descricao, rack, nivel, quantidade, quantidade_minima } = req.body;
+
+  const sql = `
+    UPDATE produtos
+    SET codigo=?, descricao=?, rack=?, nivel=?, quantidade=?, quantidade_minima=?
+    WHERE id=?
+  `;
+
+  db.query(
+    sql,
+    [codigo, descricao, rack, nivel, quantidade, quantidade_minima, id],
+    (err) => {
+
+      if (err) {
+        return res.status(500).json({ error: "Erro ao atualizar produto" });
+      }
+
+      res.json({ sucesso: true });
+
+    }
+  );
+
+});
+
+/* ===============================
+   EXCLUIR PRODUTO
+================================ */
+
+app.delete("/produtos/:id", (req, res) => {
+
+  const { id } = req.params;
+
+  db.query(
+  "DELETE FROM produtos WHERE id = ?",
+  [id],
+    (err) => {
+
+      if (err) {
+        return res.status(500).json({ error: "Erro ao excluir produto" });
+      }
+
+      res.json({ sucesso: true });
+
+    }
+  );
+
+});
+
+/* ===============================
+   PORTA
+================================ */
+
